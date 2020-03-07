d47b35da429554fe5a10d729d6e23058c96b90e1
OnlinePage <61045650+OnlinePage@users.noreply.github.com>
2020-03-08 01:48:16 +0530

    new upload

---
+++ assets/customjs/rightclick.js
@@ -0,0 +1,34 @@
+
+
+//Disable right mouse click Script
+//By Geek Site.in
+
+
+var message="Hello there due to copyright reasons , this function is blocked";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
